bd319063752e90d8f451f63b613892451803672e
Raj Mohan <rajmohan@hotmail.com>
2022-09-09 05:47:18 -0400

    Excel All cells

--- lib/utils/excel.dart
+++ lib/utils/excel.dart
@@ -8,15 +8,38 @@ import 'package:open_filex/open_filex.dart';
 import 'package:path_provider/path_provider.dart';
 
 class NExcel {
+  static _addCell(Sheet sheet, int c, int r, dynamic v) {
+    final cell = CellIndex.indexByColumnRow(columnIndex: c, rowIndex: r);
+    sheet.cell(cell).value = v;
+  }
+
   static export(List<Student> sl) async {
     final st = Stopwatch()..start();
 
-    final excel = Excel.createExcel();
+    final Excel excel = Excel.createExcel();
     final String ds = excel.getDefaultSheet()!;
-    final sheet = excel[ds];
+    final Sheet sheet = excel[ds];
 
-    int row = 1;
+    int row = 0;
     for (var s in sl) {
+      _addCell(sheet, 0, row, s.batch);
+      _addCell(sheet, 1, row, s.schoolName);
+      _addCell(sheet, 2, row, s.gradeName);
+      _addCell(sheet, 3, row, s.sectionName);
+      _addCell(sheet, 4, row, s.roomNo);
+      _addCell(sheet, 5, row, s.studentID);
+      _addCell(sheet, 6, row, s.studentFirstName);
+      _addCell(sheet, 7, row, s.studentLastName);
+      _addCell(sheet, 8, row, s.status);
+
+      _addCell(sheet, 9, row, s.age);
+      _addCell(sheet, 10, row, s.userName);
+      _addCell(sheet, 11, row, s.parent1Name);
+      _addCell(sheet, 12, row, s.parent2Name);
+      _addCell(sheet, 13, row, s.parentEmailId);
+      _addCell(sheet, 14, row, s.parentAltEmailId);
+
+      /*
       final c1 = CellIndex.indexByColumnRow(columnIndex: 1, rowIndex: row);
       sheet.cell(c1).value = s.studentFirstName;
 
@@ -25,6 +48,7 @@ class NExcel {
 
       final c3 = CellIndex.indexByColumnRow(columnIndex: 3, rowIndex: row);
       sheet.cell(c3).value = s.studentID;
+      */
 
       row++;
     }
